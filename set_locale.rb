8059ce1609bdfd78cf6bde3e0e233e4207e62fc3
takke <takke30@gmail.com>
2018-12-13 23:42:02 +0900

    set_locale が機能しているか確かめる

--- set_locale.rb
+++ set_locale.rb
@@ -1,3 +1,11 @@
+print "before: "
+print Encoding.default_external
+print "\n"
+
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 #config.i18n.fallbacks = [I18n.default_locale]
+
+print "after: "
+print Encoding.default_external
+print "\n"
