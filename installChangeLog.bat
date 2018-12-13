d3ca919ea827b7fe71a1c83a546fe382bf6507d8
takke <takke30@gmail.com>
2018-12-13 13:12:26 +0900

    beta に戻す

--- installChangeLog.bat
+++ installChangeLog.bat
@@ -4,6 +4,6 @@ setlocal
 
 call %~dp0env-set.bat
 
-gem install github_changelog_generator --version 1.15.0.pre.rc
-rem gem install github_changelog_generator --version 1.15.0.pre.beta
+rem gem install github_changelog_generator --version 1.15.0.pre.rc
+gem install github_changelog_generator --version 1.15.0.pre.beta
 endlocal
