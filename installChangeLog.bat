f09b856ee4ddbc151f0f731b116d3b2b8f5c8fa7
takke <takke30@gmail.com>
2020-05-04 14:23:31 +0900

    use 1.15.2

--- installChangeLog.bat
+++ installChangeLog.bat
@@ -6,7 +6,7 @@ call %~dp0env-set.bat
 
 REM faraday 0.16 でエラーが発生するのでバージョンを固定する
 REM https://github.com/sakura-editor/changelog-sakura/issues/26
-call gem install faraday --version 0.15.4
+REM call gem install faraday --version 0.15.4
 
-call gem install github_changelog_generator --version 1.15.0.pre.beta
+call gem install github_changelog_generator --version 1.15.2
 endlocal
